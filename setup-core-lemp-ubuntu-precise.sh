0abe4a30f6ca06d0063071698a25bb3c9191ab20
James Bradach <jb@jamesbradach.com>
2014-05-28 19:29:04 -0500

    changed MariaDB version to 10.0

--- setup-core-lemp-ubuntu-precise.sh
+++ setup-core-lemp-ubuntu-precise.sh
@@ -17,7 +17,7 @@ fi
 # Update packages and add MariaDB repository
 echo -e '\n[Package Updates]'
 apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
-add-apt-repository 'deb http://ftp.osuosl.org/pub/mariadb/repo/5.5/ubuntu precise main'
+add-apt-repository 'deb http://ftp.osuosl.org/pub/mariadb/repo/10.0/ubuntu precise main'
 apt-get update
 apt-get -y upgrade
 
